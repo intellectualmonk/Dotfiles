f143b28237e9fce6a687013179ed6a307175db27
Morgareth99 <thiagors1983@gmail.com>
2017-08-05 22:02:56 -0300

    :construction:

---
+++ Arch/Scripts/gbackup.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+# Usage: gbackup.pl <user>
+# Clone all the gists of a GitHub user
+
+use strict;
+use warnings;
+
+use Git::Raw;
+use Pithub::Gists;
+
+my $gist = Pithub::Gists -> new;
+my $user = shift or die 'Provide a user name';
+
+my $gists = $gist -> list(user => $user);
+
+while (my $row = $gists -> next) {
+	print "Cloning gist '" . $row -> {'id'} . "'...";
+	Git::Raw::Repository -> clone($row -> {'git_pull_url'}, $row -> {'id'}, {});
+	print " done.\n";
+}
