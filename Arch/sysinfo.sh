df508510dd52791b43b848a8df3007516d82f8a2
Morgareth99 <thiagors1983@gmail.com>
2017-09-18 00:45:14 -0300

    :construction:

--- Arch/sysinfo.sh
+++ Arch/sysinfo.sh
@@ -7,7 +7,7 @@ KERNEL=$(hostnamectl | awk -F- '/Kernel/{ OFS="-";NF--; print }'|awk '{print $3}
 ARCH=$(uname -m)
 UPTIME=$(awk '{printf("%d:%02d:%02d:%02d",($1/60/60/24),($1/60/60%24),($1/60%60),($1%60))}' /proc/uptime)
 MODEL=$(cat /sys/devices/virtual/dmi/id/board_{name,vendor} | awk '!(NR%2){print$1,p}{p=$0}')
-DE=$(wmctrl -m | awk 'NR==1 {print $2}')
+DE=$(wmctrl -m | awk 'NR==1 {print $2}' | perl -nle 'print ucfirst lc')
 CPU=$(awk < /proc/cpuinfo '/model name/{gsub(/[(TMR)]/,"");print $4,$5,$6,$8}' | head -1)
 GPU=$(lspci|awk '/VGA/{print $5,$8,$9}' | tr -d '[]')
 SHELL=$(echo "$SHELL" | awk -F/ '{for ( i=1; i <= NF; i++) sub(".", substr(toupper($i),1,1) , $i); print $NF}')
@@ -15,15 +15,14 @@ RESOLUTION=$(xdpyinfo | awk '/dimensions:/ {printf $2}')
 BIRTH=$(ls -alct /|sed '$!d'|awk '{print $7, $6, $8}')
  
  
-#gtkrc="$HOME/.gtkrc-2.0"
 GtkTheme=$(awk < .gtkrc-2.0 -F'"' '/gtk-theme-name/{print $2}')
 GtkIcon=$(awk < .gtkrc-2.0 -F'"' '/gtk-icon-theme-name/{print $2}' )
 GtkFont=$(awk < .gtkrc-2.0 -F'"' '/gtk-font-name/{print $2}')
  
  
-Artist=$(mpc current --format "%artist%")
-SongTitle=$(mpc current --format "%title%")
-Album=$(mpc current --format "%album%")
+Packages=$(pacman -Q | awk 'END {print NR}')
+Layout=$(setxkbmap -print | awk -F"+" '/xkb_symbols/{for ( i=1; i <= NF; i++) sub(".", substr(toupper($i),1,1) , $i); print $2}')
+Monitor=$(awk < /var/log/Xorg.0.log '/Display/{print $6,$7}'|tr -d '()'|sed -n '1p')
  
  
  echo  ""
@@ -37,7 +36,7 @@ Album=$(mpc current --format "%album%")
  echo -e "\e[94m     \e[39m$KERNEL"
  echo -e "\e[94m     \e[39m$UPTIME"
  echo -e "\e[94m     \e[39m$SHELL"
- echo -e "\e[94m     \e[39m$RESOLUTION"
+ echo -e "\e[94m     \e[39m$RESOLUTION"
  echo -e "\e[94m     \e[39m$CPU"
  echo -e "\e[94m     \e[39m$GPU"
  echo -e "\e[92m   --------------------"
@@ -47,7 +46,10 @@ Album=$(mpc current --format "%album%")
  echo -e "\e[92m     \e[39m$GtkFont"
  echo -e "\e[92m     \e[39m$BIRTH"
  echo -e "\e[91m   --------------------"
- echo -e "\e[91m     $Artist"
- echo -e "\e[91m     $SongTitle"
- echo -e "\e[91m     $Album"
+ echo -e "\e[91m     \e[31m$Packages"
+ echo -e "\e[91m     \e[39m$Layout"
+ echo -e "\e[91m     \e[39m$Monitor"
  echo  ""
+
+
+
