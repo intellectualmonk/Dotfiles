26444044264082a731dffc8095e0910463b5cd48
Morgareth99 <thiagors1983@gmail.com>
2017-09-28 23:18:04 -0300

    :construction:

---
+++ Arch/.xmonad/scripts/weather.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+import urllib.request
+
+city = "Uberlandia"
+api_key = "642a76e68929c32f7bdee0ed86ed30a3"
+
+weather = eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}".format(city, api_key)).read())[2:-1])
+
+info = weather["weather"][0]["description"].capitalize()
+temp = int(float(weather["main"]["temp"]) - 272.15)
+
+print("%s, %i°C" % (info, temp))
+
