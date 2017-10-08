4b178978acc13b89a12e6812c39277c01791c323
Morgareth99 <thiagors1983@gmail.com>
2017-10-08 22:57:13 +0000

    :construction:

--- Arch/.config/polybar/mail.py
+++ Arch/.config/polybar/mail.py
@@ -2,6 +2,6 @@
 
 import imaplib
 obj = imaplib.IMAP4_SSL('imap.gmail.com',993)
-obj.login('mail','user') # write your email and password
+obj.login('mail','pass') # write your email and password
 obj.select()
 print(len(obj.search(None, 'UnSeen')[1][0].split()))
