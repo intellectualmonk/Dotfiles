0f84929f8b31a9cfd44ee154681e9bb40e17a38b
Morgareth99 <thiagors1983@gmail.com>
2017-12-18 18:16:26 +0000

    :construction:

--- Arch/.config/polybar/weather.py
+++ Arch/.config/polybar/weather.py
@@ -1,12 +1,21 @@
-#!/bin/python3
-import urllib.request
+#!/bin/python
+# -*- coding: utf-8 -*-
 
-CITY = "Uberlandia"
+import requests
+
+CITY = "3445831"
 API_KEY = "642a76e68929c32f7bdee0ed86ed30a3"
+UNITS = "Metric"
+UNIT_KEY = "C"
 
-WEATHER=eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}".format(CITY, API_KEY)).read())[2:-1])
-
-INFO = WEATHER["weather"][0]["description"].capitalize()
-TEMP = int(float(WEATHER["main"]["temp"]) - 272.15)
-
-print("%s, %i°C" % (INFO, TEMP))
+REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&appid={}&units={}".format(CITY, API_KEY, UNITS))
+try:
+    # HTTP CODE = OK
+    if REQ.status_code == 200:
+        CURRENT = REQ.json()["weather"][0]["description"].capitalize()
+        TEMP = int(float(REQ.json()["main"]["temp"]))
+        print("{}, {} °{}".format(CURRENT, TEMP, UNIT_KEY))
+    else:
+        print("Error: BAD HTTP STATUS CODE " + str(REQ.status_code))
+except (ValueError, IOError):
+    print("Error: Unable print the data")
